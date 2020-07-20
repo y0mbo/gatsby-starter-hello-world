7060af1c2704dbb5d98772e2a48eeac44207660d
John Uhri <john.uhri@redbitbluebit.com>
2020-07-20 10:24:54 -0500

    Running through tutorial

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header headerText="About Gatsby" />
+      <p>Such wow. Very React.</p>
+      <Header headerText="It's pretty cool" />
+
+    </div>
+  )
+}
